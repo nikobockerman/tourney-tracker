1fe3980752470e48868be7c89ad9c12af3fecc42
Niko Böckerman <git.585wj@slmail.me>
2024-12-04 19:33:41 +0000

    Remove unused RouterOutput import

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -4,7 +4,6 @@ import {
 } from '@aws-amplify/ui-angular';
 import { ChangeDetectionStrategy, Component } from '@angular/core';
 import { Amplify } from 'aws-amplify';
-import { RouterOutlet } from '@angular/router';
 
 import { MatchesComponent } from './matches/matches.component';
 
@@ -17,7 +16,7 @@ Amplify.configure(outputs);
   // Feature request for this: https://github.com/aws-amplify/amplify-ui/issues/4899
   // eslint-disable-next-line @angular-eslint/prefer-on-push-component-change-detection
   changeDetection: ChangeDetectionStrategy.Default,
-  imports: [RouterOutlet, MatchesComponent, AmplifyAuthenticatorModule],
+  imports: [MatchesComponent, AmplifyAuthenticatorModule],
   selector: 'app-root',
   styleUrl: './app.component.css',
   templateUrl: './app.component.html',
